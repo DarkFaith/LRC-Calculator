d9e7728db4d50c188aef556ae8d8ee15c5b76422
DarkFaith <darkfaith93@hotmail.com>
2014-02-27 12:51:14 -0500

    Initial commit

--- src/LRCCalculator.java
+++ src/LRCCalculator.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package lrccalculator;
+
 
 import java.io.UnsupportedEncodingException;
 import java.util.logging.Level;
